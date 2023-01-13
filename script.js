53b482383799bbb9a87d0ec27d30e5ab90c2212a
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-01-13 18:30:00 +0530

    AdvanceJs-403-03.

---
+++ script.js
@@ -0,0 +1,35 @@
+var bill = document.getElementById("bill");
+        var select = document.getElementById("select");
+        var people = document.getElementById("people");
+        var result = document.getElementById("tip");
+        function calculate() {
+            if (bill.value == "") {
+                result.innerHTML = "Please enter total bill.";
+            }
+            else {
+                if (select.value == "") {
+                    result.innerHTML = "Please choose service."
+                }
+                else {
+                    if (people.value == "") {
+                        result.innerHTML = "Please enter number of people."
+                    }
+                    else {
+                        if (bill.value != "" && select.value != "" && people.value != "") {
+                            let tip = (bill.value * select.value) / people.value;
+                            result.innerHTML = `<p><strong>Tip Amount</strong></p>
+                                 <h1>$&nbsp;${tip}</h1>
+                                 <p><strong>each</p></strong>`;
+                            bill.value = "";
+                            select.value = "";
+                            people.value = "";
+                        }
+                    }
+                }
+            }
+            result.style.display = "flex";
+            result.style.flexDirection = "column";
+            result.style.justifyContent = "center";
+            result.style.alignItems = "center";
+            
+        }
